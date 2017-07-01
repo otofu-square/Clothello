fe4102140179e258d368858f9edb590438e4b280
otofu-square <yukita@feedforce.jp>
2017-07-01 14:40:52 +0900

    :+1: Add on-command func

--- src/clothello/core.clj
+++ src/clothello/core.clj
@@ -1,6 +1,15 @@
 (ns clothello.core
   (:gen-class))
 
+(defn on-command
+  [cmdline]
+  (let [cmd (first cmdline)
+        pos (second cmdline)]
+    (cond
+      (= cmd :move) (play-move pos)
+      (= cmd :exit) (System/exit 0)
+      :else nil)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
