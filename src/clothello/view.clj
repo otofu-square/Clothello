4b486567993f72ca8bdea17afc57086090e3cc2b
otofu-square <yukita@feedforce.jp>
2017-07-12 15:57:04 +0900

    :+1: Add view.clj

--- src/clothello/view.clj
+++ src/clothello/view.clj
@@ -1,3 +1,31 @@
 (ns clothello.view
-  (:use clothello.model)
-  [clojure.string :only (join)])
+  (:use
+   clothello.model
+   [clojure.string :only (join)]))
+
+(def col-headers
+  (take b-size
+        (map (comp str char)
+             (range code-a code-z))))
+
+(def col-headers-str
+  (str "  " (join " " col-headers)))
+
+(defn- st-str
+  [st]
+  (condp = st
+    :b "x"
+    :w "o"
+    " "))
+
+(defn- board-strs
+  [brd]
+  (for [row (partition b-size brd)]
+    (join " " (map st-str row))))
+
+(defn- board-strs-with-row
+  [brd]
+  (map str
+       (range (inc first-row) (inc last-row))
+       (repeat b-size " ")
+       (board-strs brd)))
